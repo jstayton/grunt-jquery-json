41e3e5a7b9fe43c2985e55dad9d232e0edaa0645
Justin Stayton <justin.stayton@gmail.com>
2013-01-29 09:10:26 -0600

    Added Mocha tests for 'get-jquery-json' helper method.

--- test/jquery-json_test.js
+++ test/jquery-json_test.js
@@ -1,34 +1,78 @@
-var grunt = require('grunt');
+var should = require('should'),
+    grunt = require('grunt');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+grunt.loadTasks('tasks');
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+describe('jquery-json', function () {
+  var pkg = require('./fixtures/package.json'),
+      config = {};
 
-exports['plugin'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'helper': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(grunt.helper('plugin'), 'plugin!!!', 'should return the correct value.');
-    test.done();
-  }
-};
+  describe('helper', function () {
+    describe('get-jquery-json', function () {
+      it('should return an object', function () {
+        var jqueryJson = grunt.helper('get-jquery-json', pkg, config);
+
+        jqueryJson.should.be.a('object');
+      });
+
+      it('should require a "pkg" argument', function () {
+        (function () {
+          var jqueryJson = grunt.helper('get-jquery-json');
+        }).should.throw();
+      });
+
+      it('should not require a "config" argument', function () {
+        var jqueryJson = grunt.helper('get-jquery-json', pkg);
+
+        jqueryJson.should.be.a('object');
+      });
+
+      it('should include required fields', function () {
+        var jqueryJson = grunt.helper('get-jquery-json', pkg, config);
+
+        jqueryJson.should.have.property('name', 'testplugin');
+        jqueryJson.should.have.property('version', '1.2.3');
+        jqueryJson.should.have.property('title', 'jQuery Test Plugin');
+        jqueryJson.should.have.property('author').and.have.keys('name');
+        jqueryJson.should.have.property('licenses').and.have.length(1);
+        jqueryJson.should.have.property('dependencies').and.have.keys('jquery');
+      });
+
+      it('should include optional fields', function () {
+        var jqueryJson = grunt.helper('get-jquery-json', pkg, config);
+
+        jqueryJson.should.have.property('description', 'A jQuery plugin for testing.');
+        jqueryJson.should.have.property('keywords').and.have.length(3);
+        jqueryJson.should.have.property('homepage', 'https://github.com/tauthor/jquery-testplugin');
+        jqueryJson.should.have.property('docs', 'https://github.com/tauthor/jquery-testplugin/blob/master/README.md');
+        jqueryJson.should.have.property('demo', 'http://tauthor.github.com/jquery-testplugin');
+        jqueryJson.should.have.property('download', 'http://tauthor.github.com/jquery-testplugin/latest.zip');
+        jqueryJson.should.have.property('bugs', 'https://github.com/tauthor/jquery-testplugin/issues');
+        jqueryJson.should.have.property('maintainers').and.have.length(1);
+      });
+
+      it('should exclude unused fields', function () {
+        var jqueryJson = grunt.helper('get-jquery-json', pkg, config);
+
+        jqueryJson.should.not.have.property('unused');
+      });
+
+      it('should overwrite "pkg" values with "config" values', function () {
+        var config = {
+              title: 'Different Plugin Name',
+              description: 'A different description.'
+            },
+            jqueryJson = grunt.helper('get-jquery-json', pkg, config);
+
+        jqueryJson.should.have.property('title', 'Different Plugin Name');
+        jqueryJson.should.have.property('description', 'A different description.');
+      });
+
+      it('should remove "jquery" from the "name" field', function () {
+        var jqueryJson = grunt.helper('get-jquery-json', pkg, config);
+
+        jqueryJson.should.have.property('name', 'testplugin');
+      });
+    });
+  });
+});
